8685fc49bdd945a4fdb4e1b9996f77e094304b63
Adrien S <33064608+adrienst@users.noreply.github.com>
2020-04-29 22:23:36 +0200

    Add files via upload

---
+++ Recup-Logiciels.ps1
@@ -0,0 +1,23 @@
+
+#création d'un array
+$Array = @()
+
+#récupération des logiciels installés
+$logiciels = Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* 
+
+#enumération
+Foreach ($logiciel in $logiciels) {
+    #suppression des éléments ou le nom est vide
+    if ($logiciel.displayname -ne $null) {
+        #mise en forme
+        $resultat = "" | Select-object displayname, DisplayVersion, Publisher, InstallDate
+        $resultat.displayname = $logiciel.displayname
+        $resultat.DisplayVersion = $logiciel.DisplayVersion
+        $resultat.Publisher = $logiciel.Publisher
+        $resultat.InstallDate = $logiciel.InstallDate
+        #ajout dans l'array
+        $Array += $resultat
+    }
+}
+#affichage de l'array
+$Array | Sort-Object displayname 
