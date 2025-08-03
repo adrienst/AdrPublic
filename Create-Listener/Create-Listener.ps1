e2a0132a08f251ca36bc01ff3599fbe748c2cf60
Adrien S. <33064608+adrienst@users.noreply.github.com>
2025-08-03 12:30:35 +0200

    Update Create-Listener.ps1

--- Create-Listener/Create-Listener.ps1
+++ Create-Listener/Create-Listener.ps1
@@ -1,13 +1,35 @@
-$listenerParams = @{
+# Create self signed certificate
+$certParams = @{
+    CertStoreLocation = 'Cert:\LocalMachine\My'
+    DnsName           = $env:COMPUTERNAME
+    NotAfter          = (Get-Date).AddYears(1)
+    Provider          = 'Microsoft Software Key Storage Provider'
+    Subject           = "CN=$env:COMPUTERNAME"
+}
+$cert = New-SelfSignedCertificate @certParams
+
+# Create HTTPS listener
+$httpsParams = @{
     ResourceURI = 'winrm/config/listener'
     SelectorSet = @{
         Transport = "HTTPS"
         Address   = "*"
     }
-    ValueSet    = @{
-        CertificateThumbprint = 'E6CDAA82EEAF2ECE8546E05DB7F3E01AA47D76CE'
+    ValueSet = @{
+        CertificateThumbprint = $cert.Thumbprint
         Enabled               = $true
-        Port                  = 5986
     }
 }
-New-WSManInstance @listenerParams
+New-WSManInstance @httpsParams
+
+# Opens port 5986 for all profiles
+$firewallParams = @{
+    Action      = 'Allow'
+    Description = 'Inbound rule for Windows Remote Management via WS-Management. [TCP 5986]'
+    Direction   = 'Inbound'
+    DisplayName = 'Windows Remote Management (HTTPS-In)'
+    LocalPort   = 5986
+    Profile     = 'Any'
+    Protocol    = 'TCP'
+}
+New-NetFirewallRule @firewallParams
