a4820b2cf8567c8bfe8550efd5cad40c607bfb16
Adrien S. <33064608+adrienst@users.noreply.github.com>
2025-08-14 14:52:39 +0200

    Update prep-ionos.ps1

--- Prep-IONOS/prep-ionos.ps1
+++ Prep-IONOS/prep-ionos.ps1
@@ -1,5 +1,5 @@
-$urlSetTime = "https://github.com/adrienst/AdrPublic/blob/master/Set-Time/set-time.ps1"
-$urlRemoveVmware = "https://github.com/adrienst/AdrPublic/blob/master/remove-vmware/remove-vmware.ps1"
+$urlSetTime = "https://raw.githubusercontent.com/adrienst/AdrPublic/refs/heads/master/Set-Time/set-time.ps1"
+$urlRemoveVmware = "https://raw.githubusercontent.com/adrienst/AdrPublic/refs/heads/master/remove-vmware/remove-vmware.ps1"
 
 
 irm $urlSetTime | iex
